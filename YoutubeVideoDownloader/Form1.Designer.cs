be77612f722ab9837b3428fef95342275e48d0c6
Murad <85393488+MuradAga@users.noreply.github.com>
2022-02-21 12:52:22 +0400

    second commit

---
+++ YoutubeVideoDownloader/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace YoutubeVideoDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblYT = new System.Windows.Forms.Label();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.lblTittle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblYT
+            // 
+            this.lblYT.AutoSize = true;
+            this.lblYT.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblYT.Location = new System.Drawing.Point(133, 9);
+            this.lblYT.Name = "lblYT";
+            this.lblYT.Size = new System.Drawing.Size(441, 37);
+            this.lblYT.TabIndex = 0;
+            this.lblYT.Text = "YouTube/Instagram Media Yükləyici";
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblUrl.Location = new System.Drawing.Point(20, 91);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(65, 37);
+            this.lblUrl.TabIndex = 1;
+            this.lblUrl.Text = "URL";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtUrl.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUrl.Location = new System.Drawing.Point(91, 96);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(596, 32);
+            this.txtUrl.TabIndex = 2;
+            this.txtUrl.TextChanged += new System.EventHandler(this.txtUrl_TextChanged);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDownload.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDownload.Location = new System.Drawing.Point(280, 179);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(136, 47);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "Yüklə";
+            this.btnDownload.UseVisualStyleBackColor = false;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // lblTittle
+            // 
+            this.lblTittle.AutoSize = true;
+            this.lblTittle.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTittle.Location = new System.Drawing.Point(93, 135);
+            this.lblTittle.Name = "lblTittle";
+            this.lblTittle.Size = new System.Drawing.Size(0, 28);
+            this.lblTittle.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 270);
+            this.Controls.Add(this.lblTittle);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.lblUrl);
+            this.Controls.Add(this.lblYT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblYT;
+        private Label lblUrl;
+        private TextBox txtUrl;
+        private Button btnDownload;
+        private Label lblTittle;
+    }
+}
---
+++ YoutubeVideoDownloader/Form1.cs
@@ -0,0 +1,100 @@
+﻿using VideoLibrary;
+using Instadown_NetCore;
+using System.Net;
+using Newtonsoft.Json;
+using System.Drawing.Imaging;
+
+namespace YoutubeVideoDownloader
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog fbd;
+        MediaClass m;
+        string path;
+        public Form1()
+        {
+            InitializeComponent();
+            m = new MediaClass();
+            path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            fbd = new FolderBrowserDialog() { Description = "Faylın yerini seçin !"};
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Media yüklənir ...", "Məlumat", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                if (txtUrl.Text.ToString().ToLower().Contains("youtube"))
+                {
+                    YouTube yt = new YouTube();
+                    Video video = yt.GetVideo(txtUrl.Text);
+                    File.WriteAllBytes(fbd.SelectedPath + @"\" + video.FullName, video.GetBytes());
+                }
+                else
+                {
+                    m.inputUrl = txtUrl.Text;
+                    m.DownloadImage(path);
+                }
+               
+                MessageBox.Show("Media yükləndi ...", "Məlumat", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Faylın yeri seçilmədi !", "Xəta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        bool youtube = false;
+        bool instagram = true;
+        Video video;
+
+        private void txtUrl_TextChanged(object sender, EventArgs e)
+        {
+            youtube = false;
+            instagram = true;
+            try
+            {
+                if (txtUrl.Text.ToLower().Contains("youtube"))
+                {
+                    YouTube yt = new YouTube();
+                    video = yt.GetVideo(txtUrl.Text);
+                    youtube = true;
+                }
+                else
+                {
+                    m.inputUrl = txtUrl.Text;
+                    WebClient webClient = new WebClient();
+                    dynamic val = JsonConvert.DeserializeObject<object>(webClient.DownloadString(m.inputUrl + "?__a=1"));
+                }
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    m.inputUrl = txtUrl.Text.ToString();
+                    m.DownloadImage(path);
+                    instagram = true;
+                }
+                catch (Exception)
+                {
+                    instagram = false;
+                }
+            }
+            finally
+            {
+                if (youtube)
+                {
+                    lblTittle.Text = "YouTube : " + video.Title;
+                }
+                else if (instagram)
+                {
+                    lblTittle.Text = "Instagram Media";
+                }
+                else
+                {
+                    lblTittle.Text = "Media tapılmadı...";
+                }
+            }
+        }
+    }
+}
